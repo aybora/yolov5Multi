0b045ae5bfb3477b3af4aa72711e4e898fbe6125
Aybora Köksal <aybora@metu.edu.tr>
2021-04-21 15:06:26 +0300

    yolov3like loss

--- utils/loss.py
+++ utils/loss.py
@@ -170,7 +170,7 @@ class ComputeLoss:
 
         g = 0.5  # bias
         off = torch.tensor([[0, 0],
-                            [1, 0], [0, 1], [-1, 0], [0, -1],  # j,k,l,m
+                            # [1, 0], [0, 1], [-1, 0], [0, -1],  # j,k,l,m
                             # [1, 1], [1, -1], [-1, 1], [-1, -1],  # jk,jm,lk,lm
                             ], device=targets.device).float() * g  # offsets
 
@@ -192,8 +192,8 @@ class ComputeLoss:
                 gxi = gain[[2, 3]] - gxy  # inverse
                 j, k = ((gxy % 1. < g) & (gxy > 1.)).T
                 l, m = ((gxi % 1. < g) & (gxi > 1.)).T
-                j = torch.stack((torch.ones_like(j), j, k, l, m))
-                t = t.repeat((5, 1, 1))[j]
+                j = torch.stack((torch.ones_like(j),))
+                t = t.repeat((off.shape[0], 1, 1))[j]
                 offsets = (torch.zeros_like(gxy)[None] + off[:, None])[j]
             else:
                 t = targets[0]
